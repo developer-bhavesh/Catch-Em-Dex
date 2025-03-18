93ec1ae7a919b31f665bbde65f7813bd1a184b75
developer-bhavesh <solankibhavesh1304@gmail.com>
2025-03-18 12:59:44 +0530

    Initial Commit

---
+++ Catch`Em Dex/Catch_Em_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catch_Em_DexApp.swift
+//  Catch`Em Dex
+//
+//  Created by Teacher on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Catch_Em_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
